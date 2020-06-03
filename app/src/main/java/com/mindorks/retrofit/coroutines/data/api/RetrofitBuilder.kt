4c497eba56edfa0aa535232ad1af28a580ea85ee
961155 <=>
2020-06-03 17:37:35 -0500

    add interceptor

--- app/src/main/java/com/mindorks/retrofit/coroutines/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/mindorks/retrofit/coroutines/data/api/RetrofitBuilder.kt
@@ -1,5 +1,7 @@
 package com.mindorks.retrofit.coroutines.data.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,10 +9,14 @@ object RetrofitBuilder {
 
     private const val BASE_URL = "https://5e510330f2c0d300147c034c.mockapi.io/"
 
+    val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    val httpClientBuilder = OkHttpClient.Builder().addInterceptor(loggingInterceptor)
+
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClientBuilder.build())
             .build()
     }
 
